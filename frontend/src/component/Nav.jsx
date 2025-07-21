f906326244c710766184195109e486b75dbaaffd
Ekta-Priya <ektapriya36@gmail.com>
2025-07-21 18:08:51 +0530

    Update Nav.jsx

--- frontend/src/component/Nav.jsx
+++ frontend/src/component/Nav.jsx
@@ -25,8 +25,8 @@ function Nav(){
         try{
             const result = await axios.get(serverUrl+"/api/auth/logout",{withCredentials:true})
             console.log(result.data)
-            getCurrentUser()
-
+            setUserData(null);
+            navigate("/login");
         }
         catch(error){
             console.log("error",error)
