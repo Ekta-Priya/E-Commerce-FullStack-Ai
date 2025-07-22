5ed356addafa6ddc73174a0ca81c49f45117878a
Ekta-Priya <ektapriya36@gmail.com>
2025-07-22 10:40:34 +0530

    Update Collections.jsx

--- frontend/src/pages/Collections.jsx
+++ frontend/src/pages/Collections.jsx
@@ -74,12 +74,12 @@ function Collections() {
 
     return (
         <div className="w-[99vw] min-h-[100vh] bg-gradient-to-l from-[#141414] to-[#0c2025] flex items-start flex-col md:flex-row justify-start pt-[70px] overflow-x-hidden z-[2] pb-[110px]">
-            <div className={md:w-[30w] lg:w-[20vw] w-[100vw] md:min-h-[100vh] ${showFilter ? "h-[45vh]" : "h-[8vh]"} p-[20px] border-r-[1px] border-gray-400 text-[#aaf5fa] lg:fixed}>
+            <div className={`md:w-[30w] lg:w-[20vw] w-[100vw] md:min-h-[100vh] ${showFilter ? "h-[45vh]" : "h-[8vh]"} p-[20px] border-r-[1px] border-gray-400 text-[#aaf5fa] lg:fixed`}>
                 <p className="text-[25px] font-semibold flex gap-[5px] items-center justigy-start cursor-pointer" onClick={() => setShowFilter(prev => !prev)}>FILTERS
                     {!showFilter && <FaAngleDoubleRight className="text-[18px] md:hidden" />}
                     {showFilter && <FaAngleDown className="text-[18px] md:hidden" />}
                 </p>
-                <div className={border-[2px] border-[#dedcdc] pl-5 py-3 mt-6 rounded-md bg-slate-600 ${showFilter ? "" : "hidden"} md:block}>
+                <div className={`border-[2px] border-[#dedcdc] pl-5 py-3 mt-6 rounded-md bg-slate-600 ${showFilter ? "" : "hidden"} md:block`}>
                     <p className="text-[18px] text-[#f8fafa]">CATEGORIES</p>
                     <div className="w-[230px] h-[120px] flex flex-col items-start justify-center gap-[10px]">
 
@@ -99,7 +99,7 @@ function Collections() {
                     </div>
                 </div>
 
-                <div className={border-[2px] border-[#dedcdc] pl-5 py-3 mt-6 rounded-md bg-slate-600 ${showFilter ? "" : "hidden"} md:block}>
+                <div className={`border-[2px] border-[#dedcdc] pl-5 py-3 mt-6 rounded-md bg-slate-600 ${showFilter ? "" : "hidden"} md:block`}>
                     <p className="text-[18px] text-[#f8fafa]">SUBCATEGORIES</p>
                     <div className="w-[230px] h-[120px] flex flex-col items-start justify-center gap-[10px]">
 
